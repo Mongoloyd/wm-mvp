383ef6d5fafdce867fc7e45d50ff9ddc8eb39d95
Mongo <82308093+Mongoloyd@users.noreply.github.com>
2026-04-10 16:54:50 -0400

    Revert "Add ArbitrageEngine and ForensicShift demo previews with lead qualification flow"

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -22,7 +22,6 @@ import MarketMakerManifesto from "@/components/MarketMakerManifesto";
 import StickyRecoveryBar from "@/components/StickyRecoveryBar";
 import InteractiveDemoScan from "@/components/InteractiveDemoScan";
 import ArbitrageEngine from "@/components/ArbitrageEngine";
-import ForensicShift from "@/components/ForensicShift";
 import ExitIntentPhoneModal from "@/components/ExitIntentPhoneModal";
 import ScamConcernImage from "@/components/ScamConcernImage";
 import StickyCTAFooter from "@/components/StickyCTAFooter";
@@ -249,7 +248,6 @@ const Index = () => {
                 <InteractiveDemoScan onScanClick={() => triggerTruthGate('demo_scan')} />
               </XRayScannerBackground>
               <ArbitrageEngine onStartCertifiedAudit={() => triggerTruthGate("arbitrage_engine_cta")} />
-              <ForensicShift onStartCertifiedAudit={() => triggerTruthGate("forensic_shift_cta")} />
               <TruthGateFlow
                 onLeadCaptured={(sid) => { setLeadCaptured(true); setSessionId(sid); }}
                 onStepChange={(step, county) => { setStepsCompleted(step); setSelectedCounty(county); }}
