74664d45d534313d9380f094d09ccec9438a021d
Mongo <82308093+Mongoloyd@users.noreply.github.com>
2026-04-13 20:32:49 -0400

    Add DevTesting2 component with ForensicShiftDemoSection
    Create page devtesting2 with forensicshift demo
    
    Signed-off-by: Mongo <82308093+Mongoloyd@users.noreply.github.com>

---
+++ src/pages/DevTesting2.tsx
@@ -0,0 +1,13 @@
+import ForensicShiftDemoSection from "@/components/ForensicShiftDemoSection";
+
+export default function DevTesting2() {
+  return (
+    <div className="min-h-screen bg-[#0a0a0f] py-10">
+      <section className="w-full px-4 xl:px-8">
+        <div className="mx-auto max-w-[1600px]">
+          <ForensicShiftDemoSection />
+        </div>
+      </section>
+    </div>
+  );
+}
